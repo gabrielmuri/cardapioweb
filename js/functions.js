91e0bf03d07295385c00ddf7b8be2d206cd03690
Gabriel Muri <65788953+gabrielmuri@users.noreply.github.com>
2021-08-04 20:30:12 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -12,23 +12,38 @@ function incluirItem(){
 }
 
 function somaQuantidade() {
+    var valor = 5
+    document.getElementById('vlrsubtot').innerHTML = valor
     vlrQuantidade = document.getElementById('qntd').value
     btn = document.getElementById('btnSoma')
 
     btn.addEventListener('click', function(){
         valor = vlrQuantidade
         document.getElementById('qntd').value = parseInt(valor) + 1
+        somaValor()
     })
 
 }
 
 function diminuiQuantidade() {
+    var valor = 5
+    document.getElementById('vlrsubtot').innerHTML = valor
     vlrQuantidade = document.getElementById('qntd').value
     btn = document.getElementById('btnDiminui')
 
     btn.addEventListener('click', function(){
         valor = vlrQuantidade
         document.getElementById('qntd').value = parseInt(valor) - 1
+        somaValor()
     })
 
 }
+
+function somaValor() {
+    document.getElementById('vlrsubtot').innerHTML = valor
+    var valor = 5;
+    var subtot = document.getElementById('qntd').value * valor;
+    document.getElementById('vlrsubtot').innerHTML = subtot 
+}
+
+somaValor()
