e6379abde25cfccf33631ce64dc652b11fecd157
Gabriel Muri <65788953+gabrielmuri@users.noreply.github.com>
2021-08-04 12:47:26 -0300

    Update functions.js
    Adicionado Validação do campo Quantidade no cardapio

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,34 @@
 function incluirItem(){
-    window.confirm("Deseja realmente incluir este item ao seu pedido?");
+    var quantidade = document.getElementById('qntd').value
+    if ( quantidade == 0 || quantidade == '' ) {
+        alert("Por favor informe a quantidade desejada!")
+        document.getElementById('qntd').focus()
+    } else {
+        if ( confirm("Deseja realmente incluir este item ao seu pedido?") == true ) {
+            alert("Item incluido ao pedido com sucesso!")
+            window.location.href = "itens.html"
+        }
+    }
+}
+
+function somaQuantidade() {
+    vlrQuantidade = document.getElementById('qntd').value
+    btn = document.getElementById('btnSoma')
+
+    btn.addEventListener('click', function(){
+        valor = vlrQuantidade
+        document.getElementById('qntd').value = parseInt(valor) + 1
+    })
+
+}
+
+function diminuiQuantidade() {
+    vlrQuantidade = document.getElementById('qntd').value
+    btn = document.getElementById('btnDiminui')
+
+    btn.addEventListener('click', function(){
+        valor = vlrQuantidade
+        document.getElementById('qntd').value = parseInt(valor) - 1
+    })
+
 }
