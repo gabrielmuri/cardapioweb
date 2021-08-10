43299937dd47fb7b47dc036aa75de25b67435d8d
Gabriel Muri <65788953+gabrielmuri@users.noreply.github.com>
2021-08-10 20:39:27 -0300

    Add files via upload

---
+++ js/mascaras.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.cpfPessoa').mask('000.000.000-00')
+})
+$(document).ready(function() {
+    $('#telPessoa').mask('(00) 00000-0000')
+})
+$(document).ready(function() {
+    $('#user').mask('000.000.000-00')
+})
