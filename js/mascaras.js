8591f41029e954dc1b3de23645da69a2d9a2c97c
Gabriel Muri <65788953+gabrielmuri@users.noreply.github.com>
2021-08-11 18:17:32 -0300

    Delete js directory

--- js/mascaras.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.cpfPessoa').mask('000.000.000-00')
-})
-$(document).ready(function() {
-    $('#telPessoa').mask('(00) 00000-0000')
-})
-$(document).ready(function() {
-    $('#user').mask('000.000.000-00')
-})
